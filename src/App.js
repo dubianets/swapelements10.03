b1e351a46f80cb2ae3a11898a4b7b04ac0f610d4
dubianets <vadikdubianets@gmail.com>
2020-10-03 21:05:34 -0700

    flip

--- src/App.js
+++ src/App.js
@@ -12,37 +12,117 @@ const initialIteamList2 = [
   {id: 6, user: 'Masha', gender: 'Female', active: true}
 ]
 
+const initialIteamList3 = [
+  {id: 7, user: 'Lisa', gender: 'Female', active: true},
+  {id: 8, user: 'Sonia', gender: 'Female', active: true},
+  {id: 9, user: 'Marta', gender: 'Female', active: true}]
+
+const initialIteamList4 = [
+  {id: 10, user: 'Pety', gender: 'Male', active: true},
+  {id: 11, user: 'Vova', gender: 'Male', active: true},
+  {id: 12, user: 'Misha', gender: 'Male', active: true}]
+
 function App() {
 
   const [list, setList] = useState(initialIteamList);
   const [list2, setList2] = useState(initialIteamList2)
+  const [list3, setList3] = useState(initialIteamList3)
+  const [list4, setList4] = useState(initialIteamList4)
 
-  const moveElementToAnotherList = (id) => {
-    const newElem = list.find( el => el.id === id)
-    setList2([...list2, newElem]);
+  const moveElementToAnotherListDown = (id,value) => {
+    const newElem2 = value.find( el => el.id === id)
+    const newElem = {...newElem2, active: true}
+    switch (value) {
+      case list :
+        setList2([...list2, newElem]);
 
-    setList(list.filter(el => el.id !== id))
+        setList(list.filter(el => el.id !== id));break;
+      case list2 :
+        setList3([...list3, newElem]);
+
+        setList2(list2.filter(el => el.id !== id));break;
+      case list3 :
+        setList4([...list4, newElem]);
+
+        setList3(list3.filter(el => el.id !== id));break;
+    }
   }
 
-  const moveElementToAnotherList2 = (id) => {
-    const newElem = list2.find( el => el.id === id)
-    setList([...list, newElem]);
+  const moveElementToAnotherListUp = (id,value) => {
+    const newElem2 = value.find( el => el.id === id)
+    const newElem = {...newElem2, active: true}
+    switch (value) {
+      case list2 :
+        setList([...list, newElem]);
 
-    setList2(list2.filter(el => el.id !== id))
+        setList2(list2.filter(el => el.id !== id));break;
+      case list3 :
+        setList2([...list2, newElem]);
+
+        setList3(list3.filter(el => el.id !== id));break;
+      case list4 :
+        setList3([...list3, newElem]);
+
+        setList4(list4.filter(el => el.id !== id));break;
+    }
   }
+
+  const showsButtonsHandler = (id,value) => {
+    const arr = value.map(el => {
+      if(el.id === id) return {...el, active: !el.active};
+      return el;})
+
+    switch (value) {
+      case list :
+        setList(arr);break;
+      case list2 :
+        setList2(arr);break;
+      case list3 :
+        setList3(arr);break;
+      case list4 :
+        setList4(arr);break;
+    }
+  }
+
 
   return (
     <div >
 
-      {list.map(el => <li>
+      {list.map(el =>
+          <li>
         {el.user} --- {el.gender}
-        <button onClick={() => moveElementToAnotherList(el.id)}>Move element</button>
+            {el.active && <button onClick={() => showsButtonsHandler(el.id,list)}>Move element</button>}
+            {!el.active &&<button onClick={() => moveElementToAnotherListDown(el.id,list)}>down</button>}
       </li>)}
 
       <hr/>
 
-      {list2.map(el => <li>
-        {el.user} --- {el.gender} <button onClick={() => moveElementToAnotherList2(el.id)}>Move element</button>
+      {list2.map(el =>
+          <li>
+        {el.user} --- {el.gender}
+            {el.active && <button onClick={() => showsButtonsHandler(el.id,list2)}>Move element</button>}
+            {!el.active && <button onClick={() => moveElementToAnotherListUp(el.id,list2)}>up</button>}
+            {!el.active && <button onClick={() => moveElementToAnotherListDown(el.id,list2)}>down</button>}
+      </li>)}
+
+      <hr/>
+
+      {list3.map(el =>
+          <li>
+        {el.user} --- {el.gender}
+            {el.active &&  <button onClick={() => showsButtonsHandler(el.id,list3)}>Move element</button>}
+            {!el.active && <button onClick={() => moveElementToAnotherListUp(el.id,list3)}>up</button>}
+            {!el.active &&  <button onClick={() => moveElementToAnotherListDown(el.id,list3)}>down</button>}
+      </li>)}
+
+      <hr/>
+
+      {list4.map(el =>
+          <li>
+        {el.user} --- {el.gender}
+              {el.active &&  <button onClick={() => showsButtonsHandler(el.id,list4)}>Move element</button>}
+              {!el.active &&  <button onClick={() => moveElementToAnotherListUp(el.id,list4)}>up</button>}
+
       </li>)}
 
     </div>
